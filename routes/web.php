812edaf91573e0ce8869e2e04a4aae9d723f59e2
Daniel <olmosbritodaniel5@gmail.com>
2025-10-17 20:56:04 -0400

    Ciclo 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inicio', [DashboardController::class, 'index'])->name('dashboard.index');
