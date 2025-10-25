b5d982bd25025e61158c4f721237c60d6b67655c
Daniel <olmosbritodaniel5@gmail.com>
2025-10-25 00:27:49 -0400

    Actualizacion controlador

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/docente.php';
 require __DIR__.'/coordinador.php';
+
+
 // Ruta temporal para debug
 Route::get('/debug-auth', function() {
     if (auth()->check()) {
